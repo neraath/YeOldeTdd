0af1cf0e4f62f2b9439e3afb662b23713aed5123
Chris Weldon <chris@chrisweldon.net>
2011-02-09 23:00:56 -0600

    Initial project structure setup. Console initially working.

---
+++ src/Improving.YeOldTdd.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Improving.YeOldeTdd
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Welcome to Ye Olde Battleground =====");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ test/Improving.YeOldeTdd.Console.Tests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Improving.YeOldeTdd.Console.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
