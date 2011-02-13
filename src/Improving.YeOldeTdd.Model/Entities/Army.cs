1493c9d50d203b9faa604bca9a7297943816bcf9
Chris Weldon <chris@chrisweldon.net>
2011-02-13 16:46:46 -0600

    Creating Army and ArmyTests.

---
+++ src/Improving.YeOldeTdd.Model/Entities/Army.cs
@@ -0,0 +1,15 @@
+﻿namespace Improving.YeOldeTdd.Model.Entities
+{
+    using System;
+
+    public class Army
+    {
+        public bool WasAttacked { get; set; }
+
+        public void Attack(Army enemy)
+        {
+            if (enemy == null) throw new ArgumentNullException("enemy");
+            enemy.WasAttacked = true;
+        }
+    }
+}
---
+++ test/Improving.YeOldeTdd.Model.Tests/Entities/ArmyTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Improving.YeOldeTdd.Model.Tests.Entities
+{
+    using System;
+
+    using Improving.YeOldeTdd.Model.Entities;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ArmyTests
+    {
+        [TestMethod]
+        public void TestArmyCanAttack()
+        {
+            // Arrange.
+            Army army = new Army();
+            Army enemyArmy = new Army();
+
+            // Act.
+            army.Attack(enemyArmy);
+
+            // Assert.
+            Assert.IsTrue(enemyArmy.WasAttacked);
+        }
+
+        [TestMethod]
+        public void TestArmyThrowsExceptionWhenArmyIsNull()
+        {
+            // Arrange.
+            Army army = new Army();
+            bool exceptionCaught = false;
+
+            // Act.
+            try
+            {
+                army.Attack(null);
+            }
+            catch (ArgumentNullException e)
+            {
+                exceptionCaught = true;
+            }
+
+            // Assert.
+            Assert.IsTrue(exceptionCaught);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestArmyThrowsExceptionWhenArmyIsNullShort()
+        {
+            Army army = new Army();
+            army.Attack(null);
+        }
+    }
+}
